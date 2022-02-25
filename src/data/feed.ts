a4bf1b5524f31582ab35eed11f63f9b2eeec379b
Younès Bessa <bessa460@gmail.com>
2022-02-25 23:15:18 +0100

    🔥 remove tags from feed data

--- src/data/feed.ts
+++ src/data/feed.ts
@@ -7,14 +7,12 @@ const feed: Feed[] = [
     desc: "c chaud",
     link: ROUTES.PROJECTS_INSTAGRAM_DARK,
     thumbnail_url: "/static/images/logo/onruntime-white-background-low.png",
-    tags: ["Infinity War", "Parabellum", "Call of Duty"],
   },
   {
     title: "C'est la deuxième carte",
     desc: "Juste pour tester",
     link: ROUTES.PROJECTS_INSTAGRAM_DARK,
     thumbnail_url: "/static/images/logo/onruntime-white-background-low.png",
-    tags: ["Test", "Tag", "Truc de chose"],
   },
 ];
 
