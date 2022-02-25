2bb4e67758b0754f6cdfeddc3dc1317109bdc485
Antoine Kingue <etorefr@gmail.com>
2022-02-25 12:42:46 +0100

    ✨ add map

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,5 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+
+import { MAPBOX_TOKEN } from "@services/mapbox";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
 const Map: React.FC = () => {
-  return <>map</>;
+  mapboxgl.accessToken = MAPBOX_TOKEN;
+
+  const mapRef = React.useRef<HTMLDivElement>(null);
+  const [map, setMap] = React.useState<mapboxgl.Map | null>(null);
+
+  React.useEffect(() => {
+    const currentMap = new mapboxgl.Map({
+      container: mapRef.current || "",
+      style: "mapbox://styles/mapbox/dark-v10",
+      center: [2.35, 48.85],
+      zoom: 5,
+    });
+
+    currentMap.addControl(new mapboxgl.NavigationControl(), "bottom-left");
+    setMap(currentMap);
+  }, []);
+
+  return <Container ref={mapRef} />;
 };
+
+const Container = styled.div`
+  position: relative;
+  height: 100vh;
+  width: 100%;
+`;
 
 export default Map;
