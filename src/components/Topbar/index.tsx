7b166f153fb9cea629b8da9ec7698e5e4ba32762
m02xime <maxkb02@gmail.com>
2022-02-25 15:05:38 +0100

    logo on topbar

--- src/components/Topbar/index.tsx
+++ src/components/Topbar/index.tsx
@@ -2,11 +2,19 @@ import styled from "styled-components";
 import Link from "../Link";
 import ROUTES from "@constants/routes";
 import APP from "@constants/app";
+import Image from "next/image";
+import Sitelogo from "../../../public/static/images/logo/warslive.png";
 
 const Navbar: React.FC = () => {
   return (
     <Container>
       <Section>
+        <Image
+          src={Sitelogo}
+          alt="logo of the site"
+          width="30px"
+          height="30px"
+        />
         <Link href={ROUTES.HOME}>
           <BrandTitle>{APP.NAME}</BrandTitle>
         </Link>
