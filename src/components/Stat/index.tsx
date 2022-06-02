45f19931248d4c741ac4fc0ce7ed5fe605e1f5ff
alexis Mouchon <alexismouchon09@gmail.com>
2022-06-02 13:24:51 +0200

    ✨ add stat and liveFeed

---
+++ src/components/Stat/index.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+const Stat: React.FC = () => {
+  return (
+    <Container>
+      <Value>0.90$/L</Value>
+      <TitleStat>Petrol</TitleStat>
+    </Container>
+  );
+};
+
+const Container = styled.section`
+  margin-right: 5px;
+  background-color: #232323;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 60px;
+  height: 60px;
+`;
+
+const Value = styled.p`
+  color: red;
+  font-size: 15px;
+`;
+
+const TitleStat = styled.h3`
+  font-size: 10px;
+`;
+
+export default Stat;
